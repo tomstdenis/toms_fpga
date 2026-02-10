53ce686a7c496be5616e446302357b7f03c9d195
Tom St Denis <tstdenis82@gmail.com>
2026-02-10 15:30:59 -0500

    tidy up IRQ test
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- lib/useq/test2.s
+++ lib/useq/test2.s
@@ -1,12 +1,12 @@
-; Test sequence #1 for useq_tb.v (i_port should be assigned 8'hAB, [*] denotes 2 cycle
+; Test sequence #2 for useq_tb.v (i_port should be assigned 8'h00, [*] denotes 2 cycle
 .ORG 00
-LDIB F					; A = 0F
+LDIB F					; A = 0F, PC = 01
 LDIT 7					; A = 7F, PC = 02
-SEI						; enable interrupts
-INC
-JSR 1
+SEI						; enable interrupts, PC = 03
+INC						; A = 80, PC = 04
+JSR 1					; PC = 05
 .ORG 10
-JSR 1
+JSR 1					; PC = 10
 .ORG F0
-DEC
-RTI
+DEC						; PC = F0
+RTI						; PC = F1
