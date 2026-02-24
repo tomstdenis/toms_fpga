aae5577a0e3d1982adeac212ed253be620b03816
Tom St Denis <tstdenis82@gmail.com>
2026-02-23 19:57:06 -0500

    fix uart echo to use proper states for reading/writing
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- icesugar5k/demos/uart_echo/echo.v
+++ icesugar5k/demos/uart_echo/echo.v
@@ -9,7 +9,7 @@ module top(
 	wire pll_lock;
 	
 	reg rst_n;
-	wire [15:0] bauddiv = 31_875_000 / 115_200;
+	wire [15:0] bauddiv = 48_000_000 / 115_200;
 	reg uart_tx_start;
 	reg uart_rx_read;
 	reg [7:0] uart_tx_data_in;
@@ -34,15 +34,43 @@ module top(
 		end
 	end
 
+	reg [1:0] state;
+	
+	localparam
+		STATE_IDLE=0,
+		STATE_WAIT_READ=1,
+		STATE_WRITE_BYTE=2,
+		STATE_WAIT_WRITE=3;
 
 	always @(posedge pll_clk) begin
-		if (uart_rx_ready) begin
-			uart_rx_read <= 1;
-			uart_tx_data_in <= uart_rx_byte;
-			uart_tx_start <= 1;
-		end else begin
-			uart_rx_read <= 0;
-			uart_tx_start <= 0;
+		if (rst_n) begin
+			case(state)
+				STATE_IDLE:
+					begin
+						if (uart_rx_ready) begin
+							// there's a byte to read
+							uart_rx_read <= 1'b1;
+							state <= STATE_WAIT_READ;
+						end
+					end
+				STATE_WAIT_READ:
+					begin
+						// waiting for read to ack byte
+						uart_rx_read <= 1'b0;				// only want 1 byte from the RX fifo
+						state <= STATE_WRITE_BYTE;			// next cycle we'll have the byte
+					end
+				STATE_WRITE_BYTE:
+					begin
+						uart_tx_data_in <= uart_rx_byte;	// copy RX byte to TX 
+						uart_tx_start <= 1'b1;				// issue write
+						state <= STATE_WAIT_WRITE;			// next cycle is waiting for the write to finish
+					end
+				STATE_WAIT_WRITE:
+					begin
+						uart_tx_start <= 1'b0;				// only want to write 1 byte
+						state <= STATE_IDLE;				// back to idle
+					end
+			endcase
 		end
 	end
 endmodule
