c64b0bc5dfb41b763ed08cd48cfa968c4a15ef80
Tom St Denis <tstdenis82@gmail.com>
2026-03-21 18:32:30 -0400

    add config mode to esp32 code
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

---
+++ esp32/wifi_to_serial/pc/client.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <time.h>
+
+static int set_interface_attribs(int fd, int speed) {
+    struct termios tty;
+    if (tcgetattr(fd, &tty) != 0) return -1;
+
+    // cfmakeraw sets the terminal to a state where bytes are
+    // passed exactly as received: no echo, no translations, no signals.
+    cfmakeraw(&tty);
+
+    // Set baud rate
+    cfsetospeed(&tty, speed);
+    cfsetispeed(&tty, speed);
+
+    // 8-bit chars, enable receiver, ignore modem control lines
+    tty.c_cflag &= ~CSIZE & ~HUPCL;
+    tty.c_cflag |= CS8 | CREAD | CLOCAL;
+
+    // Setup timing: non-blocking read with 0.5s timeout
+    tty.c_cc[VMIN]  = 0;
+    tty.c_cc[VTIME] = 10;
+
+    if (tcsetattr(fd, TCSANOW, &tty) != 0) return -1;
+    return 0;
+}
+
+#define CFG_ENTER_BYTES_SIZE 4
+const unsigned char cfg_enter_bytes[CFG_ENTER_BYTES_SIZE] = {
+    0x1a, 0x50, 0xfd, 0xef
+};
+
+int main(int argc, char **argv)
+{
+	unsigned char buf[256];
+    int fd = open(argv[1], O_RDWR | O_NOCTTY);
+    if (fd < 0) { perror("Open port"); return 1; }
+    set_interface_attribs(fd, B1000000);
+	tcflush(fd, TCIOFLUSH);
+	
+	// program SSID
+	buf[0] = 0;
+	strcpy(buf+1, argv[2]);
+	if (write(fd, buf, 1+strlen(buf+1)) < 0) {
+		printf("Could not write to serial..\n");
+		exit(-1);
+	}
+	tcdrain(fd);
+	
+	// program PSK
+	buf[0] = 1;
+	strcpy(buf+1, argv[3]);
+	if (write(fd, buf, 1+strlen(buf+1)) < 0) {
+		printf("Could not write to serial..\n");
+		exit(-1);
+	}
+	tcdrain(fd);
+
+	// Store and reboot
+	buf[0] = 2;
+	if (write(fd, buf, 1) < 0) {
+		printf("Could not write to serial..\n");
+		exit(-1);
+	}
+	tcdrain(fd);
+}
--- esp32/wifi_to_serial/wifi_to_serial.ino
+++ esp32/wifi_to_serial/wifi_to_serial.ino
@@ -1,35 +1,119 @@
 // this works fine on my ESP32-C3 "NoName" Super Mini 
 #include <WiFi.h>
+#include <EEPROM.h>
 
 int port = 8888;  //Port number
 WiFiServer server(port);
 
 //Server connect to WiFi Network
-const char *ssid = "";  //Enter your wifi SSID
-const char *password = "";  //Enter your wifi Password
+#define STRLEN 128
+char ssid[STRLEN];  //Enter your wifi SSID
+char password[STRLEN];  //Enter your wifi Password
+char server_loaded;
+
+#define EEPROM_MAGIC          0xAA
+#define EEPROM_SSID_OFFSET    1
+#define EEPROM_PSK_OFFSET     (1 + STRLEN)
+
+enum cfg_commands {
+  CFG_COMMAND_SET_SSID = 0,
+  CFG_COMMAND_SET_PSK = 1,
+  CFG_COMMAND_SET_STORE = 2,
+};
+
+#define PIN_SETUP 0
+
+// try to load settings from EEPROM
+int load_eeprom(void)
+{
+  EEPROM.begin(512);
+  if (EEPROM.read(0) == EEPROM_MAGIC) {
+    Serial.println("Valid EEPROM magic in load...");
+    EEPROM.readBytes(EEPROM_SSID_OFFSET, ssid, STRLEN);
+    EEPROM.readBytes(EEPROM_PSK_OFFSET, password, STRLEN);
+    EEPROM.end();
+    return 0;
+  }
+  Serial.println("no valid EEPROM magic in load...");
+  return -1;
+}
+
+// store settings in eeprom
+void save_eeprom(void)
+{
+  EEPROM.begin(512);
+  Serial.println("Storing EEPROM...");
+  EEPROM.write(0, EEPROM_MAGIC);
+  EEPROM.writeBytes(EEPROM_SSID_OFFSET, ssid, STRLEN);
+  EEPROM.writeBytes(EEPROM_PSK_OFFSET, password, STRLEN);
+  EEPROM.end();
+}
+
+// read a string upto STRLEN-1 bytes (can terminate early with NUL)
+void read_string(char *p)
+{
+  int x;
+  memset(p, 0, STRLEN);
+  while (x < STRLEN-1) {
+    char ch;
+    while (!Serial.available());
+    ch = Serial.read();
+    if (!ch) {
+      return;
+    }
+    p[x++] = ch;
+  }
+}
 
 //=======================================================================
 //                    Power on setup
 //=======================================================================
 void setup() 
 {
-  Serial.begin(921600);
+  Serial.begin(1000000);
   Serial.setRxBufferSize(128);
   Serial.setTxBufferSize(128);
-  Serial.println();
+  
+  memset(ssid, 0, sizeof ssid);
+  memset(password, 0, sizeof password);
 
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password); //Connect to wifi
- 
-  // Wait for connection  
-  while (WiFi.status() != WL_CONNECTED) {   
-    delay(500);
-    delay(500);
-  }
-
-  server.begin();
   pinMode(BUILTIN_LED, OUTPUT);
+  pinMode(PIN_SETUP, INPUT_PULLUP);
   digitalWrite(BUILTIN_LED, HIGH);
+  server_loaded = 0;
+
+  // try to load from EEPROM
+  Serial.println("Booting...");
+  if (load_eeprom() == 0) {
+    Serial.println("Read settings from EERPOM trying to connect...");
+    Serial.print("SSID: [");
+    Serial.print(ssid);
+    Serial.println("]");
+    Serial.print("Password: [");
+    Serial.print(password);
+    Serial.println("]");
+    int tries = 15;
+    WiFi.begin(ssid, password); //Connect to wifi
+  
+    // Wait for connection  
+    while (WiFi.status() != WL_CONNECTED) {   
+      delay(1000);
+      Serial.write('.');
+      if (!--tries) {
+        break;
+      }
+    }
+
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("Connected.");
+      server.begin();
+      server_loaded = 1;
+    } else {
+      Serial.println("Not connected.");
+    }
+  } else {
+  }
 }
 //=======================================================================
 //                    Loop
@@ -37,31 +121,50 @@ void setup()
 
 void loop() 
 {
-  unsigned char buf[64], x;
+  unsigned char buf[256];
+  int x, y;
 
-  WiFiClient client = server.available();
-  
-  if (client) {
-    if(client.connected())
-    {
-      digitalWrite(BUILTIN_LED, LOW);
-    }
-    while(client.connected()){      
-      while((x = client.available())>0){
-        // read data from the connected client
-        client.read(buf, x);
-        Serial.write(buf, x); 
-      }
-      //Send Data to connected client
-      while((x = Serial.available())>0)
+  if (1 || digitalRead(PIN_SETUP) == HIGH) {
+    WiFiClient client = server.available();
+    
+    if (client) {
+      if(client.connected())
       {
-        Serial.read(buf, x);
-        client.write(buf, x);
+        digitalWrite(BUILTIN_LED, LOW);
       }
+      while(client.connected()){      
+        while((x = client.available())>0){
+          // read data from the connected client
+          client.read(buf, x);
+          Serial.write(buf, x); 
+        }
+        //Send Data to connected client
+        while((x = Serial.available())>0)
+        {
+          // if service mode is disabled just copy from one to the other in bulk
+          Serial.read(buf, x);
+          client.write(buf, x);
+        }
+      }
+      client.stop();
+    } else {
+      digitalWrite(BUILTIN_LED, HIGH);
     }
-    client.stop();
   } else {
-    digitalWrite(BUILTIN_LED, HIGH);
+    // handle configuration commands
+    while (!Serial.available());
+    switch (Serial.read()) {
+      case CFG_COMMAND_SET_PSK:
+        read_string(password);
+        break;
+      case CFG_COMMAND_SET_SSID:
+        read_string(ssid);
+        break;
+      case CFG_COMMAND_SET_STORE:
+        save_eeprom();
+        ESP.restart();
+        break;
+    }
   }
 }
 //=======================================================================
