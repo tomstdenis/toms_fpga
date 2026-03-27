bdece5082ff4ad8f5336138499bfb13e9d13655b
Tom St Denis <tstdenis82@gmail.com>
2026-03-27 12:56:17 -0400

    sort out pin/colouring so you can get white, red, green, blue
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- nano1k/demos/yod_pla/pc/loader.c
+++ nano1k/demos/yod_pla/pc/loader.c
@@ -143,22 +143,34 @@ int main(int argc, char **argv)
 	// use gpio[7:4] as inputs, in particular gpio[7:6] are attached to the nano20k buttons
 	struct fuses *f = create_fuse();
 
-	// out[0] = gpio[7]
+/* truth table I ended up with (UP means being pulled up either the button isn't pressed or pin28 is floating not connected to GND)
+ * PBA  | PIN 28 | RGB |
+ * --------------------
+ * UP   | UP     | RGB |
+ * UP   | DOWN   | xGx |
+ * DOWN	| UP     | Rxx |
+ * DOWN | DOWN   | xxB |
+ */
+
+	// out[0] = gpio[7] // pin28 breakout controls red
 	f->and_fuses[AND(0, 7, 0)] = 0; // (recall they come in a, ~a pairs, also 0 means to include
 	f->or_fuses[OR(0, 0)] = 1;		// use AND[0]
+	f->or_invert_fuses[0] = 1;		// (button defaults to high)
 	
-	// out[1] = gpio[7] ^ gpio[6] (7 & !6) | (!7 & 6)
+	// out[1] = ~gpio[7] ^ gpio[6] (~7 & ~6) | (7 & 6) // XOR of push button A and pin28 controls blue
 	// let's use AND[1..2] and OR[1] for this
-	f->and_fuses[AND(1, 7, 0)] = 0; // select gpio[7]
+	f->and_fuses[AND(1, 7, 1)] = 0; // select gpio[7]
 	f->and_fuses[AND(1, 6, 1)] = 0; // select ~gpio[6]
-	f->and_fuses[AND(2, 7, 1)] = 0; // select ~gpio[7]
+	f->and_fuses[AND(2, 7, 0)] = 0; // select ~gpio[7]
 	f->and_fuses[AND(2, 6, 0)] = 0; // select gpio[6]
 	f->or_fuses[OR(1, 1)] = 1;		// select AND[1]
 	f->or_fuses[OR(1, 2)] = 1;		// select AND[2]
+	f->or_invert_fuses[1] = 1;		// LED is reversed so 0 == on
 	
-	// out[2] = gpio[6]
-	f->and_fuses[AND(2, 6, 0)] = 0;	// select only gpio[6]
-	f->or_fuses[OR(2, 2)]      = 1; // select AND[2]
+	// out[2] = gpio[6] // push button A controls green
+	f->and_fuses[AND(3, 6, 0)] = 0;	// select only gpio[6]
+	f->or_fuses[OR(2, 3)]      = 1; // select AND[3]
+	f->or_invert_fuses[2] = 1;		// LED is reversed so 0 == on
 
 	upload_program(fd, f);
 }
