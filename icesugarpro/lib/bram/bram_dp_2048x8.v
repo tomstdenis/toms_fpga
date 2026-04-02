bf5645be53772de8d1fa65a8612ec1f2e969fcf1
Tom St Denis <tstdenis82@gmail.com>
2026-04-01 21:55:29 -0400

    start bram lib for ecp5 using oss
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

---
+++ icesugarpro/lib/bram/bram_dp_2048x8.v
@@ -0,0 +1,68 @@
+module bram_dp_2048x8 (
+    // Port A
+    input         clk_a,
+    input  [10:0] addr_a,
+    input  [7:0]  din_a,
+    input         we_a,
+    output [7:0]  dout_a,
+
+    // Port B
+    input         clk_b,
+    input  [10:0] addr_b,
+    input  [7:0]  din_b,
+    input         we_b,
+    output [7:0]  dout_b
+);
+
+    DP16K #(
+        .DATA_WIDTH_A(9),
+        .DATA_WIDTH_B(9),
+        .WRITEMODE_A("NORMAL"),
+        .WRITEMODE_B("NORMAL"),
+        .SELECT_REG_A("NOREG"), // Set to "REG" for +1 cycle latency but better timing
+        .SELECT_REG_B("NOREG"),
+        .GSR("DISABLED")
+    ) mem_inst (
+        // Port A Connections
+        .CLKA(clk_a),
+        .CEA(1'b1),
+        .OCEA(1'b1),
+        .WEA(we_a),
+        .RSTA(1'b0),
+        // Address: For 9-bit mode, bits [10:0] map to ADA[13:3]
+        .ADA13(addr_a[10]), .ADA12(addr_a[9]), .ADA11(addr_a[8]),
+        .ADA10(addr_a[7]),  .ADA9(addr_a[6]),   .ADA8(addr_a[5]),
+        .ADA7(addr_a[4]),   .ADA6(addr_a[3]),   .ADA5(addr_a[2]),
+        .ADA4(addr_a[1]),   .ADA3(addr_a[0]),
+        .ADA2(1'b0), .ADA1(1'b0), .ADA0(1'b0),
+        // Data
+        .DIA8(1'b0),
+        .DIA7(din_a[7]), .DIA6(din_a[6]), .DIA5(din_a[5]), .DIA4(din_a[4]),
+        .DIA3(din_a[3]), .DIA2(din_a[2]), .DIA1(din_a[1]), .DIA0(din_a[0]),
+        .DOA8(), 
+        .DOA7(dout_a[7]), .DOA6(dout_a[6]), .DOA5(dout_a[5]), .DOA4(dout_a[4]),
+        .DOA3(dout_a[3]), .DOA2(dout_a[2]), .DOA1(dout_a[1]), .DOA0(dout_a[0]),
+
+        // Port B Connections
+        .CLKB(clk_b),
+        .CEB(1'b1),
+        .OCEB(1'b1),
+        .WEB(we_b),
+        .RSTB(1'b0),
+        // Address
+        .ADB13(addr_b[10]), .ADB12(addr_b[9]), .ADB11(addr_b[8]),
+        .ADB10(addr_b[7]),  .ADB9(addr_b[6]),   .ADB8(addr_b[5]),
+        .ADB7(addr_b[4]),   .ADB6(addr_b[3]),   .ADB5(addr_b[2]),
+        .ADB4(addr_b[1]),   .ADB3(addr_b[0]),
+        .ADB2(1'b0), .ADB1(1'b0), .ADB0(1'b0),
+        // Data
+        .DIB8(1'b0),
+        .DIB7(din_b[7]), .DIB6(din_b[6]), .DIB5(din_b[5]), .DIB4(din_b[4]),
+        .DIB3(din_b[3]), .DIB2(din_b[2]), .DIB1(din_b[1]), .DIB0(din_b[0]),
+        .DOB8(),
+        .DOB7(dout_b[7]), .DOB6(dout_b[6]), .DOB5(dout_b[5]), .DOB4(dout_b[4]),
+        .DOB3(dout_b[3]), .DOB2(dout_b[2]), .DOB1(dout_b[1]), .DOB0(dout_b[0])
+    );
+
+endmodule
+
---
+++ icesugarpro/lib/bram/bram_sp_2048x8.v
@@ -0,0 +1,51 @@
+module bram_sp_2048x8 (
+    input clk,
+    input [10:0] w_addr,
+    input [7:0] w_data,
+    input w_en,
+    input [10:0] r_addr,
+    output [7:0] r_data
+);
+
+    // The DP16K has 14-bit address buses (AD). 
+    // For 2048 depth (11-bit), the mapping usually shifts 
+    // based on the width. In 9-bit mode, AD[2:0] are ignored 
+    // or used for sub-byte masking. We map our 11 bits to AD[13:3].
+
+    DP16K #(
+        .DATA_WIDTH_A(9), 
+        .DATA_WIDTH_B(9),
+        .WRITEMODE_A("NORMAL"),
+        .WRITEMODE_B("NORMAL"),
+        .SELECT_REG_A("NOREG"), // Use "REG" for a registered output (better timing)
+        .GSR("DISABLED")
+    ) mem_inst (
+        // Port A: Read
+        .CLKA(clk),
+        .CEA(1'b1),
+        .OCEA(1'b1),
+        .RSTA(1'b0),
+        .ADA13(r_addr[10]), .ADA12(r_addr[9]), .ADA11(r_addr[8]), 
+        .ADA10(r_addr[7]),  .ADA9(r_addr[6]),   .ADA8(r_addr[5]), 
+        .ADA7(r_addr[4]),   .ADA6(r_addr[3]),   .ADA5(r_addr[2]), 
+        .ADA4(r_addr[1]),   .ADA3(r_addr[0]),
+        .ADA2(1'b0), .ADA1(1'b0), .ADA0(1'b0),
+        .DOA8(), // Ignored 9th bit
+        .DOA7(r_data[7]), .DOA6(r_data[6]), .DOA5(r_data[5]), .DOA4(r_data[4]),
+        .DOA3(r_data[3]), .DOA2(r_data[2]), .DOA1(r_data[1]), .DOA0(r_data[0]),
+
+        // Port B: Write
+        .CLKB(clk),
+        .CEB(1'b1),
+        .WEB(w_en),
+        .ADB13(w_addr[10]), .ADB12(w_addr[9]), .ADB11(w_addr[8]), 
+        .ADB10(w_addr[7]),  .ADB9(w_addr[6]),   .ADB8(w_addr[5]), 
+        .ADB7(w_addr[4]),   .ADB6(w_addr[3]),   .ADB5(w_addr[2]), 
+        .ADB4(w_addr[1]),   .ADB3(w_addr[0]),
+        .ADB2(1'b0), .ADB1(1'b0), .ADB0(1'b0),
+        .DIB8(1'b0), // Ignored 9th bit
+        .DIB7(w_data[7]), .DIB6(w_data[6]), .DIB5(w_data[5]), .DIB4(w_data[4]),
+        .DIB3(w_data[3]), .DIB2(w_data[2]), .DIB1(w_data[1]), .DIB0(w_data[0])
+    );
+
+endmodule
