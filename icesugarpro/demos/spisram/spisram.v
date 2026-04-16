80ff3d08870560e646cdc165378a069b6f0ec28b
Tom St Denis <tstdenis82@gmail.com>
2026-04-15 21:49:12 -0400

    use quad io commands since ESP PSRAM doesn't allow SPI ones in quad mode
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- icesugarpro/demos/spisram/spisram.v
+++ icesugarpro/demos/spisram/spisram.v
@@ -98,7 +98,7 @@ module top(input clk, inout [3:0] sio, output cs, output sck, input uart_rx, out
 `else
 			// PSRAM configuration (Some chips allow 1 Tclk between CS low but ESP-PSRAM requires 50ns)
             .DATA_WIDTH(DATA_WIDTH), .CLK_FREQ_MHZ(`FREQ), .SRAM_ADDR_WIDTH(SRAM_ADDR_WIDTH),
-            .DUMMY_BYTES(2), .CMD_READ(8'h0B), .CMD_WRITE(8'h02), .CMD_EQIO(8'h35),
+            .DUMMY_BYTES(3), .CMD_READ(8'hEB), .CMD_WRITE(8'h38), .CMD_EQIO(8'h35),
             .MIN_CPH_NS(50), .SPI_TIMER_BITS(4), .QPI_TIMER_BITS(1), .MIN_WAKEUP_NS(150_000),
             .PSRAM_RESET(1), .CMD_RESETEN(8'h66), .CMD_RESET(8'h99)
 `endif  
