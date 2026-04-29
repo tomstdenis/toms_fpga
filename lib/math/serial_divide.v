b7767a2f9abf927afadc3392a629caca5275abd8
Tom St Denis <tstdenis82@gmail.com>
2026-04-29 19:55:09 -0400

    add serial divider to the library
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

---
+++ lib/math/serial_divide.v
@@ -0,0 +1,95 @@
+`timescale 1ns/1ps
+`default_nettype none
+
+module serial_divide
+	#(parameter
+		BIT_WIDTH=16
+	)(
+
+	input wire clk,
+	input wire rst_n,
+	
+	input wire [BIT_WIDTH-1:0] num,
+	input wire [BIT_WIDTH-1:0] denom,
+	input wire valid,
+	
+	output reg ready,
+	output reg [BIT_WIDTH-1:0] quotient,
+	output reg [BIT_WIDTH-1:0] remainder);
+	
+	reg [BIT_WIDTH-1:0] num_l;
+	reg [BIT_WIDTH-1:0] denom_l;
+	
+	reg [BIT_WIDTH-1:0] tmp;
+	
+	reg [1:0] fsm_state;
+		
+	localparam
+		FSM_IDLE      = 0,
+		FSM_DONE      = 1,
+		FSM_NORMALIZE = 2,
+		FSM_REDUCE    = 3;
+	
+	always @(posedge clk) begin
+		if (!rst_n) begin
+			tmp       <= 0;
+			quotient  <= 0;
+			remainder <= 0;
+			ready     <= 0;
+			fsm_state <= FSM_IDLE;
+		end else begin
+			case(fsm_state)
+				FSM_IDLE:
+					begin
+						if (valid) begin
+							quotient  <= 0;
+							remainder <= 0;
+							num_l     <= num;
+							denom_l   <= denom;
+							tmp	      <= 1;
+							if (num < denom || denom == 0) begin
+								remainder <= denom > 0 ? num : 0;
+								ready     <= 1;
+								fsm_state <= FSM_DONE;
+							end else begin
+								fsm_state <= FSM_NORMALIZE;
+							end
+						end	
+					end
+				FSM_NORMALIZE:
+					begin
+						// how many times can we left shift denom_l until it's bigger than num_l
+						if ({denom_l, 1'b0} <= {1'b0, num_l}) begin
+							denom_l <= {denom_l[BIT_WIDTH-2:0], 1'b0};
+							tmp     <= {tmp[BIT_WIDTH-2:0], 1'b0};
+						end else begin
+							fsm_state <= FSM_REDUCE;
+						end
+					end
+				FSM_REDUCE:
+					begin
+						if (denom_l <= num_l) begin
+							quotient <= quotient + tmp;
+							num_l    <= num_l - denom_l;
+						end else begin
+							denom_l <= {1'b0, denom_l[BIT_WIDTH-1:1]};
+							tmp <= {1'b0, tmp[BIT_WIDTH-1:1]};
+							if (tmp == 1) begin
+								remainder <= num_l;
+								ready     <= 1;
+								fsm_state <= FSM_DONE;
+							end
+						end
+					end
+				FSM_DONE:
+					begin
+						if (!valid) begin
+							ready     <= 0;
+							fsm_state <= FSM_IDLE;
+						end
+					end
+				default: fsm_state <= FSM_IDLE;
+			endcase
+		end
+	end
+endmodule
---
+++ lib/math/serial_divide_tb.v
@@ -0,0 +1,81 @@
+`timescale 1ns/1ps
+
+module serial_divide_tb();
+	reg clk;
+	reg rst_n;
+
+	localparam
+		BIT_WIDTH=16;
+		
+	reg [BIT_WIDTH-1:0] num;
+	reg [BIT_WIDTH-1:0] denom;
+	reg valid;
+	
+	wire [BIT_WIDTH-1:0] quotient;
+	wire [BIT_WIDTH-1:0] remainder;
+	wire ready;
+	
+	serial_divide #(.BIT_WIDTH(BIT_WIDTH)) serial_divider (
+		.clk(clk),
+		.rst_n(rst_n),
+		.num(num), .denom(denom), .valid(valid),
+		.ready(ready), .quotient(quotient), .remainder(remainder));
+
+    // Parameters for the simulation
+    localparam CLK_PERIOD = 20; // 50MHz Clock
+    // Clock Generation
+    always #(CLK_PERIOD/2) clk = ~clk;
+    
+    integer i;
+	
+	initial begin
+        // Waveform setup
+        $dumpfile("serial_divide.vcd");
+        $dumpvars(0, serial_divide_tb);
+		clk = 0;
+		valid = 0;
+		num = 0;
+		denom = 0;
+
+        // Reset system
+        repeat(3) @(posedge clk);
+        rst_n = 1;
+		@(posedge clk); #1;
+		
+		// divide 27 by 4
+		num = 27;
+		denom = 4;
+		valid = 1;
+		wait(ready == 1); #1;
+		valid = 0;
+		@(posedge clk); #1;
+		if (quotient != 6) begin
+			$display("Quotient not 6 ... %d (dude I'm not mad I'm disappointed.)", quotient);
+			$fatal;
+		end
+		if (remainder != 3) begin
+			$display("Dude, like not cool.  The remainder ain't supposed to be no %d", remainder);
+			$fatal;
+		end
+		
+		// normally at this point testing would be considered complete.  But for the sake of being a "perfectionist"
+		// I guess we could try some random numbers
+		for (i = 0; i < 100000; i = i + 1) begin
+/* verilator lint_off WIDTHTRUNC */
+			num = $random;
+			denom = $random & ((1 << (BIT_WIDTH-8)) - 1);
+/* verilator lint_on WIDTHTRUNC */
+			valid = 1;
+			wait(ready == 1); #1;
+			valid = 0;
+			@(posedge clk); #1;
+			$display("n=%x d=%x, q=%x, r=%x", num, denom, quotient, remainder);
+			if (denom == 0) begin
+				if (quotient != 0 || remainder != 0) $fatal;
+			end else begin
+				if (quotient * denom + remainder != num) $fatal;
+			end
+		end
+		$finish;
+	end
+endmodule
