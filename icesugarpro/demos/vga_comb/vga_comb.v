a29a700a3143193dba6310772ca6f7bcc5508e9d
Tom St Denis <tstdenis82@gmail.com>
2026-05-11 20:04:42 -0400

    add lrg mode to vga demo
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- icesugarpro/demos/vga_comb/vga_comb.v
+++ icesugarpro/demos/vga_comb/vga_comb.v
@@ -63,11 +63,13 @@ module top(input clk, output reg [3:0] vga_r, output reg [3:0] vga_g, output reg
 
 	// VGA text mode driver, defaults to 80x25 using an 8x8 font
 	// notice we're scaling the font by 2 so we change the height to 16 here
+	wire lrg_mode = 1'b0;
+	
 	vga_text_driver #(.FONTHEIGHT(16)) textdrv(
 		.clk(pll_clk), .rst_n(rst_n),
 		.x(vga_x), .y(vga_y), .active_video(vga_active),
 		.rd_addr(rd_addr), .rd_data(rd_data),
-		.symbol(symbol));
+		.symbol(symbol), .lrg_mode(lrg_mode));
 
 	// So the pipe is vga() produces the timing that
 	// textdrv() uses produces the next 'symbol' that
@@ -89,41 +91,45 @@ module top(input clk, output reg [3:0] vga_r, output reg [3:0] vga_g, output reg
 				wr_en <= 0;
 			end
 			
-			// what value to write in the next cycle
-			case (wr_addr + 1)				
-				// first row (start on row 6/col 6)
-				80*5 + 5: wr_data <= 8'h54; // T			//80 * 5 + 5 is TEXTCOLS * vga_y/FONTHEIGHT + vga_x/FONTWIDTH
-				80*5 + 6: wr_data <= 8'h6F; // o
-				80*5 + 7: wr_data <= 8'h6d; // m
-				// space
-				80*5 + 9: wr_data <= 8'h77; // w
-				80*5 + 10: wr_data <= 8'h61; // a
-				80*5 + 11: wr_data <= 8'h73; // s
-				// space
-				80*5 + 13: wr_data <= 8'h68; // h
-				80*5 + 14: wr_data <= 8'h65; // e
-				80*5 + 15: wr_data <= 8'h72; // r
-				80*5 + 16: wr_data <= 8'h65; // e
-				// space 
-				80*5 + 18: wr_data <= 8'h61; // a
-				80*5 + 19: wr_data <= 8'h6e; // n
-				80*5 + 20: wr_data <= 8'h64; // d
-				// space
-				80*5 + 22: wr_data <= 8'h6d; // m
-				80*5 + 23: wr_data <= 8'h6f; // o
-				// space
-				80*5 + 25: wr_data <= 8'h69; // i
-				80*5 + 26: wr_data <= 8'h73; // s
-				//space				
-				80*5 + 28: wr_data <= 8'h63; // c
-				80*5 + 29: wr_data <= 8'h6f; // o
-				80*5 + 30: wr_data <= 8'h6f; // o
-				80*5 + 31: wr_data <= 8'h6c; // l			
-			endcase
+			if (lrg_mode) begin
+				wr_data <= wr_addr[7:0] + 1'b1;
+			end else begin
+				// what value to write in the next cycle
+				case (wr_addr + 1)				
+					// first row (start on row 6/col 6)
+					80*5 + 5: wr_data <= 8'h54; // T			//80 * 5 + 5 is TEXTCOLS * vga_y/FONTHEIGHT + vga_x/FONTWIDTH
+					80*5 + 6: wr_data <= 8'h6F; // o
+					80*5 + 7: wr_data <= 8'h6d; // m
+					// space
+					80*5 + 9: wr_data <= 8'h77; // w
+					80*5 + 10: wr_data <= 8'h61; // a
+					80*5 + 11: wr_data <= 8'h73; // s
+					// space
+					80*5 + 13: wr_data <= 8'h68; // h
+					80*5 + 14: wr_data <= 8'h65; // e
+					80*5 + 15: wr_data <= 8'h72; // r
+					80*5 + 16: wr_data <= 8'h65; // e
+					// space 
+					80*5 + 18: wr_data <= 8'h61; // a
+					80*5 + 19: wr_data <= 8'h6e; // n
+					80*5 + 20: wr_data <= 8'h64; // d
+					// space
+					80*5 + 22: wr_data <= 8'h6d; // m
+					80*5 + 23: wr_data <= 8'h6f; // o
+					// space
+					80*5 + 25: wr_data <= 8'h69; // i
+					80*5 + 26: wr_data <= 8'h73; // s
+					//space				
+					80*5 + 28: wr_data <= 8'h63; // c
+					80*5 + 29: wr_data <= 8'h6f; // o
+					80*5 + 30: wr_data <= 8'h6f; // o
+					80*5 + 31: wr_data <= 8'h6c; // l			
+				endcase
 
-			// put random data on lines 7 down
-			if (wr_addr + 1 > 7*80) begin
-				wr_data <= counter[7:0];
+				// put random data on lines 7 down
+				if (wr_addr + 1 > 7*80) begin
+					wr_data <= counter[7:0];
+				end
 			end
 		end
 	end
@@ -134,7 +140,15 @@ module top(input clk, output reg [3:0] vga_r, output reg [3:0] vga_g, output reg
 		vga_b = 0;
 		
 		if (vga_active) begin
-			{vga_r, vga_g, vga_b} = text_out ? 12'b1111_1111_1111 : 12'b0;
+			if (!lrg_mode) begin
+				{vga_r, vga_g, vga_b} = text_out ? 12'b1111_1111_1111 : 12'b1_0001_0001;
+			end else begin
+				{vga_r, vga_g, vga_b} = {
+					{ symbol[2:0], &symbol[2:0] }, 
+					{ symbol[5:3], &symbol[5:3] }, 
+					{ symbol[7:6], symbol[7:6] }
+				};
+			end
 		end
 	end
 
