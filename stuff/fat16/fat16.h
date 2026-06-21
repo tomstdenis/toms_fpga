89e36fa28a5886054ecb79523327914fc5a9ad88
Tom St Denis <tstdenis82@gmail.com>
2026-06-21 16:38:53 -0400

    tidy up
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- stuff/fat16/fat16.h
+++ stuff/fat16/fat16.h
@@ -61,38 +61,6 @@ struct fat16_volinfo {
 #define D_FZ0(fv)	  (((uint16_t)(fv->dirent[0x1D])<<8) | fv->dirent[0x1C])			
 #define D_FZ1(fv)	  (((uint16_t)(fv->dirent[0x1F])<<8) | fv->dirent[0x1E])			
 
-/*
-// a FAT16 directory entry
-struct fat16_dirent {
-	uint8_t filename[8];
-	uint8_t ext[3];
-	uint8_t attrib;
-	uint8_t res0;
-	uint8_t ctime_ms;
-	uint8_t ctime[2];								// creation time
-	uint8_t cdate[2];								// creation date
-	uint8_t ldate[2];								// last access date
-	uint8_t res1[2];
-	uint8_t lwtime[2];								// last write time
-	uint8_t lwdate[2];								// last write date
-	uint8_t scluster[2];							// starting cluster
-	uint8_t filesz[4];
-};
-
-// FAT16 directory walker structure
-struct fat16_de {
-	uint16_t cur_cluster;							// current sector we're reading from
-	uint8_t cur_sector;								// current sector # in the cluster
-	uint8_t cur_entry;								// current entry in the sector
-};
-
-// a currently open file
-struct fat16_file {
-	uint16_t scluster;								// starting cluster
-	uint16_t filesz[2];								// file size
-	uint16_t filepos[2];							// current file position
-};
-*/
 // helper functions
 void fat16_c_to_s(struct fat16_volinfo *fv, uint16_t p[2]);			// cluster to sector addressing
 void fat16_s_to_b(uint16_t p[2]);									// sector to byte addressing
@@ -102,14 +70,14 @@ void fat16_add_32(uint16_t p[2], uint16_t off[2]);					// add 32 bits to a 32-bi
 int fat16_cmp_32(uint16_t a[2], uint16_t b[2]);						// compare 32-bit a to 32-bit b
 
 // volume/FAT related
-uint16_t fat16_initvol(struct fat16_volinfo *fv, uint8_t *secbuf);		// init the volinfo structure
-uint16_t fat16_sc2dc(struct fat16_volinfo *fv, uint16_t scluster);		// convert starting cluster to data cluster
-uint16_t fat16_n_c(struct fat16_volinfo *fv, uint16_t cluster);			// find the next cluster
+uint16_t fat16_initvol(struct fat16_volinfo *fv, uint8_t *secbuf);	// init the volinfo structure
+uint16_t fat16_sc2dc(struct fat16_volinfo *fv, uint16_t scluster);	// convert starting cluster to data cluster
+uint16_t fat16_n_c(struct fat16_volinfo *fv, uint16_t cluster);		// find the next cluster
 
 // directory related
 void fat16_opendir(struct fat16_volinfo *fv, uint16_t cluster);		// open a directory
-uint16_t fat16_nextdir(struct fat16_volinfo *fv);		// walk to next entry
-uint16_t fat16_wpath(struct fat16_volinfo *fv, char *path);					// walk a file system path to a dirent
+uint16_t fat16_nextdir(struct fat16_volinfo *fv);					// walk to next entry
+uint16_t fat16_wpath(struct fat16_volinfo *fv, char *path);			// walk a file system path to a dirent
 
 // file related
 uint16_t fat16_fopen(struct fat16_volinfo *fv, char *path);
