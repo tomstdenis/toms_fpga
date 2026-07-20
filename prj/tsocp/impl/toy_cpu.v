c5bb8789b1e3a79abcf472e1691e7d10fd33ecf3
Tom St Denis <tstdenis82@gmail.com>
2026-07-20 17:11:01 -0400

    starting impl test
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

---
+++ prj/tsocp/impl/toy_cpu.v
@@ -0,0 +1,45 @@
+module top(
+    input wire clk,
+    input wire rst_n,
+
+    output wire is_halted,
+
+    // port A
+    output wire [7:0] bus_addr_a,
+    output wire [7:0] bus_data_in_a,
+    input wire [7:0] bus_data_out_a,
+    output wire bus_wr_en_a,
+    output wire bus_valid_a,
+    input wire bus_ready_a,
+
+    // port B
+    output wire [7:0] bus_addr_b,
+    output wire [7:0] bus_data_in_b,
+    input wire [7:0] bus_data_out_b,
+    output wire bus_wr_en_b,
+    output wire bus_valid_b,
+    input wire bus_ready_b
+);
+    wire pll_clk;
+    wire plllock;
+
+    pll mypll(.clkin(clk), .clkout0(pll_clk), .locked(plllock));
+
+    reg rst_n = 0;
+
+    always @(posedge pllclk) begin
+        rst_n <= 1;
+    end
+
+    toy_isa cpu(
+        .clk(clk), .rst_n(rst_n), .is_halted(is_halted),
+
+        .bus_addr_a(bus_addr_a), .bus_data_in_a(bus_data_in_a),
+        .bus_data_out_a(bus_data_out_a), .bus_wr_en_a(bus_wr_en_a),
+        .bus_valid_a(bus_valid_a), .bus_ready_a(bus_ready_a),
+
+        .bus_addr_b(bus_addr_b), .bus_data_in_b(bus_data_in_b),
+        .bus_data_out_b(bus_data_out_b), .bus_wr_en_b(bus_wr_en_b),
+        .bus_valid_b(bus_valid_b), .bus_ready_b(bus_ready_b));
+
+endmodule
