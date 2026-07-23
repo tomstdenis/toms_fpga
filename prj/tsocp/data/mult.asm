c93a426d76d00d9a40e24e30c59aa98e2f44d5ef
Tom St Denis <tstdenis82@gmail.com>
2026-07-22 21:50:22 -0400

    tidy and speed up mult demo
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- prj/tsocp/data/mult.asm
+++ prj/tsocp/data/mult.asm
@@ -1,14 +1,14 @@
 .ORG 0
 main:
-    LDi R2,operands
-    JALR mult
-    LDi R1,product
-    ST  R0,R1
+    LDi R2,operands     ; R2 = pointer to operands
+    JALR mult           ; call multiply routine
+    LDi R1,product      ; R1 = pointer to where to store product
+    ST  R0,R1           ; store product
     HALT
 
-; mult(p) p => [operands]
+; mult(p) R2 => [operands], product => R0
 mult:
-    LDi R0,temp
+    LDi R0,multtemp
     ST  R3,R0           ; save return address
     LD  R0, R2          ; R0 = Multiplier (A)
     INC R2
@@ -17,9 +17,7 @@ mult:
 
 mult_loop:
     ; Check if Multiplier (R0) is 0
-    LDi R2, 0
-    SIEQ R0, R2         ; Is R0 == 0?
-    ;AND R0,R0          ; in reality this is easier for "is zero"
+    AND R0,R0           ; in reality this is easier for "is zero"
     JZ  mult_done       ; Exit loop if R0 is zero
 
     ; Check if LSB of R0 is set using temporary AND logic
@@ -42,13 +40,13 @@ skip_add:
 mult_done:
     XOR R0,R0
     ADD R0,R3           ; R0 = R3
-    LDi R3,temp
+    LDi R3,multtemp
     LD  R3,R3           ; restore return address (LDi/LD using same reg is handy for not destroying a pointless reg)
     RET
+multtemp:
+.DB 0
 
 operands:
 .DB 7, 6                ; Inputs: 7, 6. Output placeholder: 0
 product:
 .DB 0                   ; Output placeholder: 0
-temp:
-.DB 0
