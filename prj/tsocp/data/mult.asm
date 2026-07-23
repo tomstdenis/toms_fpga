0b3eedfdf053818dc54db9a4b7611e30db32ca1f
Tom St Denis <tstdenis82@gmail.com>
2026-07-23 15:39:50 -0400

    tidy up mult demo to use new opcodes better
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- prj/tsocp/data/mult.asm
+++ prj/tsocp/data/mult.asm
@@ -15,30 +15,20 @@ mult:
     LD  R1, R2          ; R1 = Multiplicand (B)
     LDi R3, 0           ; R3 = Accumulator (Product)
 
-mult_loop:
+mult_loop:              ; Multiply R0 by R1 => R3
     ; Check if Multiplier (R0) is 0
     AND R0,R0           ; in reality this is easier for "is zero"
     JZ  mult_done       ; Exit loop if R0 is zero
-
-    ; Check if LSB of R0 is set using temporary AND logic
-    ; (Since we need a copy of R0 to mask bit 0 without losing R0)
-    ; In this ISA, we can test odd/even by shifting right and back,
-    ; or by keeping a 1 in R2 to test bit 0:
-    LSB R0
+    LSB R0              ; ZF = LSB of R0
     JZ  skip_add        ; If LSB was 0, skip adding Multiplicand
-
     ADD R3, R1          ; Accumulator += Multiplicand
-
 skip_add:
     SHR R0              ; Multiplier >>= 1
-    
-    ; Shift Multiplicand left: R1 = R1 + R1
     ADD R1, R1          ; Multiplicand <<= 1
     JMP mult_loop
 
 mult_done:
-    XOR R0,R0
-    ADD R0,R3           ; R0 = R3
+    MOV R0,R3           ; R0 = R3
     LDi R3,multtemp
     LD  R3,R3           ; restore return address (LDi/LD using same reg is handy for not destroying a pointless reg)
     RET
