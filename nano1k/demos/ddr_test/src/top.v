64c0dda60cffbd92c757673c965cc4432b280b4e
Tom St Denis <tstdenis82@gmail.com>
2026-08-18 19:29:47 -0400

    stash nano1k ddr lab hackery
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- nano1k/demos/ddr_test/src/top.v
+++ nano1k/demos/ddr_test/src/top.v
@@ -1,4 +1,4 @@
-module top(input wire clk, output wire sdr_pin, output wire ddr_pin, output wire clk_pin);
+module top(input wire clk, output wire sdr_pin, output wire ddr_pin, output wire clk_pin, output reg [3:0] sio);
 
     Gowin_OSC almost_clock(
         .oscout(clk_pin), //output oscout
@@ -8,17 +8,32 @@ module top(input wire clk, output wire sdr_pin, output wire ddr_pin, output wire
     reg [15:0] counter;
     reg tick = 0;
 
-    assign sdr_pin = tick;
 	Gowin_DDR tommy_two_two(
 //		.din({1'b0, tick}), //input [1:0] din       THIS produces a HIGH-LOW 
-		.din({tick, 1'b0}), //input [1:0] din       THIS produces a LOW-HIGH (you want this for SPI)
+		.din({1'b1, 1'b0}), //input [1:0] din       THIS produces a LOW-HIGH (you want this for SPI)
 		.clk(clk_pin), //input clk
 		.q(ddr_pin) //output [0:0] q
 	);
 
+    assign sdr_pin = tick;
+
     always @(posedge clk_pin) begin
         counter <= counter + 1'b1;
-        tick <= ((counter == 1023) || (counter == 1025)) ? 1 : 0;
+        sio  <= 4'b0000;
+        tick <= 1'b1;
+        case (counter)
+            1023:
+                begin
+                    tick <= 1'b0;
+                    sio  <= 4'b1010;
+                end
+            1024:
+                begin
+                    tick <= 1'b0;
+                    sio  <= 4'b0101;
+                end
+            1025: tick <= 1'b0;
+        endcase
     end
 
 endmodule
