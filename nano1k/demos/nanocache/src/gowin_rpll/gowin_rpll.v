63a50209abbe45c852f054a25959a9c7b38c7fb8
Tom St Denis <tstdenis82@gmail.com>
2026-08-28 18:15:29 -0400

    restore Fmax to 74MHz
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- nano1k/demos/nanocache/src/gowin_rpll/gowin_rpll.v
+++ nano1k/demos/nanocache/src/gowin_rpll/gowin_rpll.v
@@ -4,8 +4,9 @@
 //Tool Version: V1.9.11.03 Education
 //Part Number: GW1NZ-LV1QN48C6/I5
 //Device: GW1NZ-1
-//Created Time: Fri Aug 28 16:19:01 2026
+//Created Time: Fri Aug 28 18:09:34 2026
 `default_nettype wire
+
 module Gowin_rPLL (clkout, clkin);
 
 output clkout;
@@ -39,9 +40,9 @@ rPLL rpll_inst (
 
 defparam rpll_inst.FCLKIN = "27";
 defparam rpll_inst.DYN_IDIV_SEL = "false";
-defparam rpll_inst.IDIV_SEL = 1;
+defparam rpll_inst.IDIV_SEL = 3;
 defparam rpll_inst.DYN_FBDIV_SEL = "false";
-defparam rpll_inst.FBDIV_SEL = 4;
+defparam rpll_inst.FBDIV_SEL = 10;
 defparam rpll_inst.DYN_ODIV_SEL = "false";
 defparam rpll_inst.ODIV_SEL = 8;
 defparam rpll_inst.PSDA_SEL = "0000";
