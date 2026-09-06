c70abde6423e0555175c894af2f421c382170e59
Tom St Denis <tstdenis82@gmail.com>
2026-09-06 06:43:16 -0400

    bring ecp5 vga demo up to parity with the console60k demo
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- icesugarpro/demos/vga/vga.v
+++ icesugarpro/demos/vga/vga.v
@@ -45,28 +45,58 @@ module top(
 		rst_n = 1'b0;
 	end
 	
-	reg [8:0] cnt;
+	reg [6:0] cnt;
+    reg [26:0] demo_counter;
 	always @(posedge host_clk) begin
 		if (!rst_n) begin
 			host_write_mask <= 4'b1111;
 			host_addr       <= -4;
 			host_data_in    <= 32'hFFE01C03;
-//			host_data_in    <= 32'h44434241;
+			//host_data_in    <= 32'h44434241;
 			video_mode      <= 1'b1;
 			rst_n           <= 1'b1;
 			cnt             <= 0;
+            demo_counter    <= 0;
 		end else begin
-			host_addr       <= host_addr + 16'd4;
-			cnt             <= cnt + 1;
-			if (cnt == 79) begin
-				cnt <= 0;
-			end
-			if (cnt == 0) begin
-				host_data_in <= {host_data_in[23:0], host_data_in[31:24]};
-			end
-			if (host_addr == (320 * 200) - 4) begin
-				host_write_mask <= 4'b0000;
-			end
+            demo_counter    <= demo_counter + 1;
+            if (demo_counter == 75_000_000) begin
+                demo_counter <= 0;
+                video_mode   <= ~video_mode;
+                host_addr    <= -4;
+                host_write_mask <= 4'b1111;
+                cnt          <= 0;
+                if (video_mode) begin
+                    // moving to text mode
+                    host_data_in    <= 32'hC642F941; // regular white on bright blue A and bright red on regular yellow B
+                end else begin
+                    // moving to graphical mode
+                    host_data_in    <= 32'hFFE01C03;
+                end
+            end else begin
+                host_addr       <= host_addr + 16'd4;
+                cnt             <= cnt + 1;
+                if (video_mode) begin
+                    if (cnt == 79) begin
+                        cnt <= 0;
+                    end
+                    if (host_addr == (320 * 200) - 4) begin
+                        host_write_mask <= 4'b0000;
+                    end
+                    if (cnt == 0) begin
+                        host_data_in <= {host_data_in[23:0], host_data_in[31:24]};
+                    end
+                end else begin
+                    if (cnt == 39) begin
+                        cnt <= 0;
+                    end
+                    if (host_addr == (80*25*2) - 4) begin
+                        host_write_mask <= 4'b0000;
+                    end
+                    if (cnt == 0) begin
+                        host_data_in <= {host_data_in[15:0], host_data_in[31:16]};
+                    end
+                end
+            end
 		end
 	end
 endmodule
