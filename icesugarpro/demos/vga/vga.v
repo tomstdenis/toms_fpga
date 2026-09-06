e48410c5b8eccf11f5b94088d75ef3ef3ad55d7a
Tom St Denis <tstdenis82@gmail.com>
2026-09-06 18:18:35 -0400

    bring ecp5 vga demo up to parity with console60k demo
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- icesugarpro/demos/vga/vga.v
+++ icesugarpro/demos/vga/vga.v
@@ -7,6 +7,7 @@ module top(
 	output reg  vga_v_pulse,
 	output reg  vga_h_pulse
 );
+	reg rst_n = 1'b0;
 
 	wire host_clk;
 	wire vga_clk;
@@ -25,9 +26,10 @@ module top(
 	wire [3:0]  vga_out_b;
 	wire        vga_out_v_pulse;
 	wire        vga_out_h_pulse;
+    reg         rst_vga_n = 1'b0;
 	
 	vga myvga(
-		.vga_clk(vga_clk), .host_clk(host_clk), .rst_n(rst_n),
+		.vga_clk(vga_clk), .host_clk(host_clk), .rst_n(rst_vga_n),
 		.video_mode(video_mode), .page_sel(page_sel),
 		.host_addr(host_addr), .host_data_in(host_data_in), .host_data_out(host_data_out), .host_write_mask(host_write_mask),
 		.vga_r(vga_out_r), .vga_g(vga_out_g), .vga_b(vga_out_b), .vga_v_pulse(vga_out_v_pulse), .vga_h_pulse(vga_out_h_pulse));
@@ -39,15 +41,11 @@ module top(
 		vga_b       <= vga_out_b[3:2];
 		vga_v_pulse <= vga_out_v_pulse;
 		vga_h_pulse <= vga_out_h_pulse;
-	end
-	
-	reg rst_n;
-	initial begin
-		rst_n = 1'b0;
+        rst_vga_n   <= 1'b1;
 	end
 	
 	reg [6:0] cnt;
-    reg [26:0] demo_counter;
+    reg [31:0] demo_counter;
 	always @(posedge host_clk) begin
 		if (!rst_n) begin
 			host_write_mask <= 4'b1111;
@@ -61,7 +59,7 @@ module top(
 			page_sel        <= 0;
 		end else begin
             demo_counter    <= demo_counter + 1;
-            if (demo_counter == 75_000_000) begin
+            if (demo_counter == 150_000_000) begin
                 demo_counter <= 0;
                 video_mode   <= ~video_mode;
                 host_addr    <= -4;
@@ -93,19 +91,27 @@ module top(
                     if (host_addr == (65536 + 320*200)) begin // turn off writes
                         host_write_mask <= 4'b0000;
                     end
-                    if (demo_counter == 37_500_000) begin
+                    if (demo_counter == 75_000_000) begin
 						page_sel <= ~page_sel;
 					end
                 end else begin
                     if (cnt == 39) begin
                         cnt <= 0;
                     end
-                    if (host_addr == (80*25*2) - 4) begin
-                        host_write_mask <= 4'b0000;
-                    end
                     if (cnt == 0) begin
                         host_data_in <= {host_data_in[15:0], host_data_in[31:16]};
                     end
+
+                    if (host_addr == (80*25*2 - 4)) begin
+                        host_addr    <= 65536;
+                        host_data_in <= 32'h9F44E043; // bright yellow C on black and bright blue D on white 
+                    end
+                    if (host_addr == (65536 + 80*25*2) - 4) begin
+                        host_write_mask <= 4'b0000;
+                    end
+                    if (demo_counter == 75_000_000) begin
+						page_sel <= ~page_sel;
+					end
                 end
             end
 		end
