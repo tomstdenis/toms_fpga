359e8e7dec245cb3c76f32382b113d89715aa4a0
Tom St Denis <tstdenis82@gmail.com>
2026-09-06 15:35:50 -0400

    demo page flipping in text mode also slow down demo cycle
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- console60k/demos/vga/src/vga.v
+++ console60k/demos/vga/src/vga.v
@@ -50,7 +50,7 @@ module top(
 	end
 	
 	reg [6:0] cnt;
-    reg [26:0] demo_counter;
+    reg [31:0] demo_counter;
 	always @(posedge host_clk) begin
 		if (!rst_n) begin
 			host_write_mask <= 4'b1111;
@@ -64,7 +64,7 @@ module top(
 			page_sel        <= 0;
 		end else begin
             demo_counter    <= demo_counter + 1;
-            if (demo_counter == 75_000_000) begin
+            if (demo_counter == 150_000_000) begin
                 demo_counter <= 0;
                 video_mode   <= ~video_mode;
                 host_addr    <= -4;
@@ -96,19 +96,27 @@ module top(
                     if (host_addr == (65536 + 320*200)) begin // turn off writes
                         host_write_mask <= 4'b0000;
                     end
-                    if (demo_counter == 37_500_000) begin
+                    if (demo_counter == 75_000_000) begin
 						page_sel <= ~page_sel;
 					end
                 end else begin
                     if (cnt == 39) begin
                         cnt <= 0;
                     end
-                    if (host_addr == (80*25*2) - 4) begin
-                        host_write_mask <= 4'b0000;
-                    end
                     if (cnt == 0) begin
                         host_data_in <= {host_data_in[15:0], host_data_in[31:16]};
                     end
+
+                    if (host_addr == (80*25*2 - 4)) begin
+                        host_addr    <= 65536;
+                        host_data_in <= 32'h9F44E043; // bright yellow C on black and bright blue D on white 
+                    end
+                    if (host_addr == (65536 + 80*25*2) - 4) begin
+                        host_write_mask <= 4'b0000;
+                    end
+                    if (demo_counter == 75_000_000) begin
+						page_sel <= ~page_sel;
+					end
                 end
             end
 		end
