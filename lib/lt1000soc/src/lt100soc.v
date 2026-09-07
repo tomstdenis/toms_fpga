bb12f272b21957354160537dd61fb68e2e0755d2
Tom St Denis <tstdenis82@gmail.com>
2026-09-07 18:37:10 -0400

    bit more deets
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- lib/lt1000soc/src/lt100soc.v
+++ lib/lt1000soc/src/lt100soc.v
@@ -5,16 +5,18 @@ module lt100soc
 #(
     // *** SOC parameters ***
     parameter CORE_FREQ_KHZ   = 50_000,         // 50MHz default core clock
-    parameter CACHE_SIZE_BITS = 13,             // 8KB cache for PSRAM region
+    parameter CACHE_SIZE_BITS = 12,             // 8KB cache for PSRAM region
     parameter TCM_SIZE_BITS   = 15,             // 32KB TCM region
 
     // *** UART parameters ***
-    parameter UART_BAUD     = 230_400
+    parameter UART_BAUD       = 230_400,
+    parameter UART_FIFO_DEPTH = 64,
 )
 (
     // *** Clocks ***
-    input             core_clk,                  // The clock for the CPU
-    input             vga_clk,                   // The clock for the VGA
+    input  wire       core_clk,                  // The clock for the CPU
+    input  wire       vga_clk,                   // The clock for the VGA
+    input  wire       rst_n,
 
     // *** VGA output ***
     output wire [3:0] vga_r,                     // VGA red channel
@@ -45,6 +47,147 @@ module lt100soc
     input wire        uart_rx,                   // UART RX pin
     output wire       uart_tx                    // UART TX pin
 );
+
+// memory addressing 
+localparam 
+    // regions (one hot selector)
+    MEM_16M_BIOS  = 24,
+    MEM_16M_TCM   = 25,
+    MEM_16M_VGA   = 26,
+    MEM_16M_PSRAM = 27,
+    MEM_16M_MMIO  = 28,
+
+    // MMIO address (lower 8 bits)
+    MEM_MMIO_MCFG         = 8'h00,
+    MEM_MMIO_GPIO_DOUT    = 8'h04,
+    MEM_MMIO_GPIO_DIN     = 8'h08,
+    MEM_MMIO_GPIO_OE      = 8'h0C,
+    MEM_MMIO_GPIO_SET     = 8'h10,
+    MEM_MMIO_GPIO_CLEAR   = 8'h14,
+    MEM_MMIO_GPIO_TOGGLE  = 8'h18,
+    MEM_MMIO_UART_DATA    = 8'h1C,
+    MEM_MMIO_UART_STATUS  = 8'h20,
+    MEM_MMIO_VGA_CTRL     = 8'h24,
+    MEM_MMIO_SPI_TRANSFER = 8'h28;
+
+// *** BIOS ***
+    reg [7:0] bios_lane_0[0:2047];
+    reg [7:0] bios_lane_1[0:2047];
+    reg [7:0] bios_lane_2[0:2047];
+    reg [7:0] bios_lane_3[0:2047];
+    wire [11:0] bios_mem_addr;
+    reg [7:0] bios_mem_dout0;
+    reg [7:0] bios_mem_dout1;
+    reg [7:0] bios_mem_dout2;
+    reg [7:0] bios_mem_dout3;
+    reg [7:0] bios_mem_dout0_tmp;
+    reg [7:0] bios_mem_dout1_tmp;
+    reg [7:0] bios_mem_dout2_tmp;
+    reg [7:0] bios_mem_dout3_tmp;
+    always @(posedge core_clk) begin
+        bios_mem_dout0_tmp <= bios_lane_0[bios_mem_addr];
+        bios_mem_dout1_tmp <= bios_lane_1[bios_mem_addr];
+        bios_mem_dout2_tmp <= bios_lane_2[bios_mem_addr];
+        bios_mem_dout3_tmp <= bios_lane_3[bios_mem_addr];
+        bios_mem_dout0     <= bios_mem_dout0_tmp;
+        bios_mem_dout1     <= bios_mem_dout1_tmp;
+        bios_mem_dout2     <= bios_mem_dout2_tmp;
+        bios_mem_dout3     <= bios_mem_dout3_tmp;
+    end
+    initial begin
+`include "../bios/bios_lane0.vh"
+`include "../bios/bios_lane1.vh"
+`include "../bios/bios_lane2.vh"
+`include "../bios/bios_lane3.vh"
+    end
+
+// *** TCM ***
+    reg [7:0] tcm_lane0[0:(1<<(TCM_SIZE_BITS-2))-1];
+    reg [7:0] tcm_lane1[0:(1<<(TCM_SIZE_BITS-2))-1];
+    reg [7:0] tcm_lane2[0:(1<<(TCM_SIZE_BITS-2))-1];
+    reg [7:0] tcm_lane3[0:(1<<(TCM_SIZE_BITS-2))-1];
+    reg [7:0] tcm_dout0;
+    reg [7:0] tcm_dout1;
+    reg [7:0] tcm_dout2;
+    reg [7:0] tcm_dout3;
+    reg [7:0] tcm_dout0_tmp;
+    reg [7:0] tcm_dout1_tmp;
+    reg [7:0] tcm_dout2_tmp;
+    reg [7:0] tcm_dout3_tmp;
+    reg [7:0] tcm_din0;
+    reg [7:0] tcm_din1;
+    reg [7:0] tcm_din2;
+    reg [7:0] tcm_din3;
+    reg [3:0] tcm_wren;
+    wire [TCM_SIZE_BITS-1:0] tcm_addr;
+    always @(posedge core_clk) begin
+        if (tcm_wren[0]) begin
+            tcm_lane0[tcm_addr] <= tcm_din0;
+        end else begin
+            tcm_dout0_tmp <= tcm_lane0[tcm_addr];
+            tcm_dout0     <= tcm_dout0_tmp;
+        end
+        if (tcm_wren[1]) begin
+            tcm_lane1[tcm_addr] <= tcm_din1;
+        end else begin
+            tcm_dout1_tmp <= tcm_lane1[tcm_addr];
+            tcm_dout1     <= tcm_dout1_tmp;
+        end
+        if (tcm_wren[2]) begin
+            tcm_lane2[tcm_addr] <= tcm_din2;
+        end else begin
+            tcm_dout2_tmp <= tcm_lane2[tcm_addr];
+            tcm_dout2     <= tcm_dout2_tmp;
+        end
+        if (tcm_wren[3]) begin
+            tcm_lane3[tcm_addr] <= tcm_din3;
+        end else begin
+            tcm_dout3_tmp <= tcm_lane3[tcm_addr];
+            tcm_dout3     <= tcm_dout3_tmp;
+        end
+    end
+
+// *** VGA ***
+    reg vga_video_mode;
+    reg vga_page_sel;
+    wire [16:0] vga_host_addr;
+    wire [31:0] vga_data_in;
+    wire [3:0]  vga_wren;
+    wire [31:0] vga_data_out;
+
+    vga vga(
+        .vga_clk(vga_clk), .host_clk(core_clk), .rst_n(rst_n),
+        .video_mode(vga_video_mode), .page_sel(vga_page_sel),
+        .host_addr(vga_host_addr), .host_data_in(vga_data_in),
+        .host_write_mask(vga_wren), .host_data_out(vga_data_out),
+        .vga_r(vga_r), .vga_g(vga_g), .vga_b(vga_b), .vga_v_blank(vga_v_pulse),
+        .vga_h_blank(vga_h_pulse)
+    );
+
+// *** UART ***
+    localparam
+        BAUD_DIV   = (CORE_FREQ_KHZ * 1000) / UART_BAUD,
+        BAUD_WIDTH = $clog2(BAUD_DIV);
+
+    wire [BAUD_WIDTH-1:0] uart_baud = BAUD_DIV;
+    reg        uart_tx_start;
+    reg [7:0]  uart_tx_data_in;
+    wire       uart_tx_fifo_full;
+    wire       uart_tx_fifo_empty;
+    reg        uart_rx_read;
+    wire       uart_rx_ready;
+    wire [7:0] uart_rx_byte;
+
+    uart #(
+        .FIFO_DEPTH(UART_FIFO_DEPTH), .RX_ENABLE(1), .TX_ENABLE(1), .BAUD_WIDTH(BAUD_WIDTH)
+    ) uart (
+        .clk(core_clk), .rst_n(rst_n), .baud_div(uart_baud),
+        .uart_tx_start(uart_tx_start), .uart_tx_data_in(uart_tx_data_in),
+        .uart_tx_pin(uart_tx), .uart_tx_fifo_full(uart_tx_fifo_full),
+        .uart_tx_fifo_empty(uart_tx_fifo_empty), .uart_rx_pin(uart_rx),
+        .uart_rx_read(uart_rx_read), .uart_rx_ready(uart_rx_ready),
+        .uart_rx_byte(uart_rx_byte)
+    );
 
 endmodule
 
