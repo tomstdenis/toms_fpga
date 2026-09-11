ea53e3dc849431ff2e3e6566c17c31ee3b86fe0d
Tom St Denis <tstdenis82@gmail.com>
2026-09-10 20:44:26 -0400

    smaller morse thingy
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- icesugarnano/demos/goamd/goamd.v
+++ icesugarnano/demos/goamd/goamd.v
@@ -5,13 +5,13 @@ module goamd(
 	localparam
 		FREQ    = 12_000_000,
 		ui_time = FREQ / 10,			// 100ms
-		CHAR_G   = 8'h00,
-		CHAR_O   = 8'h01,
-		CHAR_SPC = 8'h02,
-		CHAR_A   = 8'h03,
-		CHAR_M   = 8'h04,
-		CHAR_D   = 8'h05,
-		CHAR_EXC = 8'h06;
+		CHAR_G   = 3'h00,
+		CHAR_O   = 3'h01,
+		CHAR_SPC = 3'h02,
+		CHAR_A   = 3'h03,
+		CHAR_M   = 3'h04,
+		CHAR_D   = 3'h05,
+		CHAR_EXC = 3'h06;
 /* morse code primer...
     so a UI is 100ms, each letter is made up of dots and dashes
     
@@ -23,13 +23,11 @@ module goamd(
 */    
 
 	// the morse coded message "go amd!!"
-	reg [63:0] message;
-	reg [7:0] char_letter;
+	reg [23:0] message;
 	reg [7:0] char_morse;
-	reg [2:0] char_bits;
 
 	// timing
-	reg [31:0] timer;
+	reg [23:0] timer;
 	
 	// fsm
 	reg [1:0] fsm_state;
@@ -53,17 +51,14 @@ module goamd(
 				fsm_state <= STATE_NEXT_CHAR;
 			end
 			STATE_NEXT_CHAR: begin
-				char_letter <= message[63:56];
-				message     <= {message[55:0], message[63:56]};
+				message     <= {message[20:0], message[23:21]};
 				fsm_state   <= STATE_NEXT_BIT;
-				case (message[63:56])
+				case (message[23:21])
 					CHAR_G: begin // --.
-						char_morse <= 3'b011; // reversed 
-						char_bits  <= 3;
+						char_morse <= 4'b1011; // reversed 
 					end
 					CHAR_O: begin // ---
-						char_morse <= 3'b111; // reversed 
-						char_bits  <= 3;
+						char_morse <= 4'b1111; // reversed 
 					end
 					CHAR_SPC: begin // intra word gap
 						timer <= ui_time * 7;
@@ -71,20 +66,16 @@ module goamd(
 						fsm_tag   <= fsm_state;
 					end
 					CHAR_A: begin // .-
-						char_morse <= 2'b10;
-						char_bits  <= 2;
+						char_morse <= 3'b110;
 					end
 					CHAR_M: begin // --
-						char_morse <= 2'b11;
-						char_bits  <= 2;
+						char_morse <= 3'b111;
 					end
 					CHAR_D: begin // -..
-						char_morse <= 3'b001;
-						char_bits  <= 3;
+						char_morse <= 4'b1001;
 					end
 					CHAR_EXC: begin // -.-.--
-						char_morse <= 6'b110101;
-						char_bits  <= 6;
+						char_morse <= 7'b1110101;
 					end
 				endcase
 			end
@@ -92,17 +83,16 @@ module goamd(
 				// dot (0) 1UI, dash (1) 3UI, inter 1UI
 				timer      <= char_morse[0] ? (3 * ui_time) : ui_time;
 				char_morse <= {1'b0, char_morse[7:1]};
-				char_bits  <= char_bits - 1'b1;
 				led        <= 1'b1;
 				fsm_state  <= STATE_DELAY;
-				fsm_tag    <= (char_bits == 1) ? STATE_NEXT_CHAR : fsm_state;
+				fsm_tag    <= (char_morse[7:1] == 1) ? STATE_NEXT_CHAR : fsm_state;
 			end
 			STATE_DELAY: begin
 				timer <= timer - 1'b1;
 				if (timer == 1) begin
 					led <= 1'b0;
 					if (led == 1) begin // inter delay (character, or element)
-						timer <= (char_bits == 0) ? (3 * ui_time) : ui_time;
+						timer <= (char_morse == 1) ? (3 * ui_time) : ui_time;
 					end else begin
 						fsm_state <= fsm_tag;
 					end
