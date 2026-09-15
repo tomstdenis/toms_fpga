7b95a8496cf7735b276ab22098ffa9b0be43c3a4
Tom St Denis <tstdenis82@gmail.com>
2026-09-15 19:36:59 -0400

    moar IRQs ... moar
    Signed-off-by: Tom St Denis <tstdenis82@gmail.com>

--- lib/lt1000soc/demos/yield.c
+++ lib/lt1000soc/demos/yield.c
@@ -2,6 +2,18 @@
 
 uint32_t vblanks = 0;
 
+static void gpio_irq(uint32_t data)
+{
+	puts("4th GPIO Positive Edge IRQ...\n\r");
+}
+
+static void uart_irq(uint32_t data)
+{
+	puts("Returning to BIOS from IRQ...\r\n");	
+	void (*bios_entry)(void) = (void (*)(void))0x01000000;
+	bios_entry();
+}
+
 static void vblank_irq(uint32_t data)
 {
 	++vblanks;
@@ -43,27 +55,31 @@ void main(void)
 		delay_ms(500);
 	}
 	
-	if (yield_add_irq(YIELD_IRQ_TIMER, yield_usec_to_cycles() * 1000UL * 250, timer_250msec) == 0) {
-		puts("Installed 250ms timer IRQ...\n\r");
-		if (yield_add_irq(YIELD_IRQ_VBLANK, 0, vblank_irq) == 0) {
-			puts("Installed vblank IRQ...\n\r");
-			if (yield_add_irq(YIELD_IRQ_TIMER, yield_usec_to_cycles() * 3000000UL, timer_3sec) == 0) {
-				puts("Installed 3 second timer IRQ...\n\r");
-				if (yield_add_irq(YIELD_IRQ_TIMER, yield_usec_to_cycles() * 1000000UL, timer_1sec) == 0) {
-					puts("Installed 1 second timer IRQ...(hit key to exit)\n\r");
-					while (!(UART_STATUS & UART_STATUS_RX_READY)) {
-						// application loop goes here... 
-						
-						// call yield frequently to keep things moving
-						yield();
+	if (yield_add_irq(YIELD_IRQ_GPIO_POSEDGE, 1 << 4, gpio_irq) == 0) {
+		puts("Installed GPIO IRQ...\n\r");
+		if (yield_add_irq(YIELD_IRQ_UART_RX_READY, 0, uart_irq) == 0) {
+			puts("Installed UART IRQ...\n\r");
+			if (yield_add_irq(YIELD_IRQ_TIMER, yield_usec_to_cycles() * 1000UL * 250, timer_250msec) == 0) {
+				puts("Installed 250ms timer IRQ...\n\r");
+				if (yield_add_irq(YIELD_IRQ_VBLANK, 0, vblank_irq) == 0) {
+					puts("Installed vblank IRQ...\n\r");
+					if (yield_add_irq(YIELD_IRQ_TIMER, yield_usec_to_cycles() * 3000000UL, timer_3sec) == 0) {
+						puts("Installed 3 second timer IRQ...\n\r");
+						if (yield_add_irq(YIELD_IRQ_TIMER, yield_usec_to_cycles() * 1000000UL, timer_1sec) == 0) {
+							puts("Installed 1 second timer IRQ...(hit key to exit)\n\r");
+							for (;;) {
+								// app code would go here
+								
+								// call this frequently to keep things moving
+								yield();
+							}
+						}
 					}
 				}
 			}
 		}
-	} else {
-		puts("Could not install IRQ handler\n\r");
 	}
-	puts("Returning to BIOS...\r\n");	
+	puts("Could not install IRQ handler\n\rReturning to BIOS...\r\n");	
 	void (*bios_entry)(void) = (void (*)(void))0x01000000;
 	bios_entry();
 }
